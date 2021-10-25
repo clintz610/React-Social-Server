c7be2016bafb9f11d873c56230678bd01b7d3bac
Max Nguyen <max.nguyen@revature.net>
2021-10-25 14:09:42 -0700

    init commit

---
+++ src/main/java/com/revature/reverb/ReverbApplication.java
@@ -0,0 +1,13 @@
+package com.revature.reverb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/reverb/ReverbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.reverb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
