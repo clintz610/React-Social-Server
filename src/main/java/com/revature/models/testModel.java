ddd7756802c452932d7b39c7eb28636da092cffe
tbridey <taelormcbride@icloud.com>
2021-10-27 10:05:28 -0600

    started testModel for react

--- src/main/java/com/revature/models/testModel.java
+++ src/main/java/com/revature/models/testModel.java
@@ -1,5 +1,14 @@
 package com.revature.models;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class testModel {
-    
+    @Getter  @Setter
+    private String username;
+    @Getter  @Setter
+    private String fname;
+    @Getter  @Setter
+    private String lname;
+    @Getter  @Setter
+    private String password;
 }
