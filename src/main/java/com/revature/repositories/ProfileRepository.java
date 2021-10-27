c90e7cd99159ba4459fd048b95a923afed361224
Jason Oh <jkoh123@yahoo.com>
2021-10-27 12:20:38 -0400

    Created backend controller, model, repo and service for profile

---
+++ src/main/java/com/revature/repositories/ProfileRepository.java
@@ -0,0 +1,11 @@
+package com.revature.repositories;
+
+import com.revature.models.Profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+    
+}
