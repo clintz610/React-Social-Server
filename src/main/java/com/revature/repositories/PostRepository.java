2243c1bb210652acd1a79486de0e2fddfc4a8e64
Terrell Miller <Terrell.Miller@revature.net>
2021-10-26 23:03:38 -0700

    Basic setup of repositories using JPAand creation of the controller,service, and models. Will set up a one-to-many relationship between the post and comments.

---
+++ src/main/java/com/revature/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.revature.repositories;
+
+import com.revature.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
