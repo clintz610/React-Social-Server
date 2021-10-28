9788f4e3b0a7bf3f22d744c0c3a0c8836d8f212a
Michael-Kochis <rhishisikk@gmail.com>
2021-10-28 15:01:40 -0500

    Basic endpoints now registering  Urgent push

--- src/main/java/com/revature/ReverbApplication.java
+++ src/main/java/com/revature/ReverbApplication.java
@@ -1,4 +1,4 @@
-package com.revature.reverb;
+package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/revature/models/testModel.java
+++ src/main/java/com/revature/models/testModel.java
@@ -1,10 +1,21 @@
 package com.revature.models;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
 public class testModel {
+    @Id
+    @Getter  @Setter
+    private Long userID;
+
     @Getter  @Setter
     private String username;
     @Getter  @Setter
