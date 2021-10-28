9eb250fe35bc9c85d765f179ad897d80897a7c10
Max Nguyen <max.nguyen@revature.net>
2021-10-28 14:01:11 -0700

    okay okay i fixed the backend by moving a file

--- src/main/java/com/revature/ReverbApplication.java
+++ src/main/java/com/revature/ReverbApplication.java
@@ -1,11 +1,10 @@
-package com.revature.reverb;
+package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.revature.models"}) 
 public class ReverbApplication {
 
 	public static void main(String[] args) {
