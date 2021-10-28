dd85d07803e0f5c5d85cc18dcf3d193e38479ada
Terrell Miller <Terrell.Miller@revature.net>
2021-10-28 15:27:17 -0700

    unsure about what is in application main, will try to see how to fix. Told that what Chris did was not needed anymore becasue they found a work around.

--- src/main/java/com/revature/ReverbApplication.java
+++ src/main/java/com/revature/ReverbApplication.java
@@ -1,15 +1,11 @@
 package com.revature;
 
-import com.revature.models.Comment;
-import com.revature.models.Post;
-import com.revature.repositories.PostRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReverbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReverbApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(ReverbApplication.class, args);
 	}
 }
