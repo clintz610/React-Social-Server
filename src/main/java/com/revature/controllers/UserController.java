0d69301fdc5b338eb7202a4743d601094935d18b
Michael-Kochis <rhishisikk@gmail.com>
2021-10-29 09:57:00 -0500

    Sample User endpoints now fully CRUD

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -2,10 +2,7 @@ package com.revature.controllers;
 
 import com.revature.models.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -17,4 +14,33 @@ public class UserController {
         returnThis.setUsername("Poncho_Villa");
         return ResponseEntity.ok(returnThis);
     }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(
+            @RequestBody User neoUser
+    ) {
+        User returnThis = new User();
+        returnThis.setUsername("User Created: "+ neoUser.getUsername());
+        return ResponseEntity.ok(returnThis);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(
+            @PathVariable(value = "id") Long userID
+    ) {
+        User returnThis = new User();
+        returnThis.setUsername("Delete User: "+ userID);
+        return ResponseEntity.ok(returnThis);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable(value = "id") Long userID,
+            @RequestBody User neoUser
+    ) {
+        User returnThis = new User();
+        returnThis.setUsername("Update User: "+ userID);
+        return ResponseEntity.ok(returnThis);
+    }
+
 }
