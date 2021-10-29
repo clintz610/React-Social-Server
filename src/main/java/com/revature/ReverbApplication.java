d993e31ba891213ab5a3158512bbca03dda461b9
Terrell Miller <Terrell.Miller@revature.net>
2021-10-28 21:38:21 -0700

    deleted data.sql file so that main will work now.

--- src/main/java/com/revature/ReverbApplication.java
+++ src/main/java/com/revature/ReverbApplication.java
@@ -3,9 +3,12 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ReverbApplication {
 
 	public static void main(String[] args) {SpringApplication.run(ReverbApplication.class, args);
+
 	}
 }
