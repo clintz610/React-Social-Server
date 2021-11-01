d266ee1987f7f66bea63ee67b9c9c47a2407daee
xaxchkuget <xaxchkuget@gmail.com>
2021-11-01 16:40:02 -0700

    Rewrite of TestcontrollersIntegration

--- src/test/java/com/revature/controllers/TestControllerIntegration.java
+++ src/test/java/com/revature/controllers/TestControllerIntegration.java
@@ -1,21 +1,14 @@
 package com.revature.controllers;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.revature.models.Profile;
+import com.revature.repositories.ProfileRepository;
 import com.revature.ReverbApplication;
 
 @RunWith(SpringRunner.class)
@@ -23,26 +16,26 @@ import com.revature.ReverbApplication;
 //properties = {"server.port=8081",
 //		"management.server.port=8082"})
 @SpringBootTest(classes = ReverbApplication.class)
-@ActiveProfiles("reverb-backend-integration-test")
 public class TestControllerIntegration {
-	
+
 	@Autowired
-	private ProfileController profilecontroller;
-	
+	private ProfileController profilectrl;
+
+	@Autowired
+	private ProfileRepository profilereptest;
+
 	@Test
-	public void devops_profile_test() throws Exception{
-		
-		
-		Profile tester = new Profile( 1,
-	     "first_name",
-	     "last_name",
-	     "profile_img",
-	     "header_img",
-	     "about_me" );
-	
-		
-		
+	public void devops_profile_test() throws Exception {
+
+		Profile tester = new Profile(1, "first_name", "last_name", "profile_img", "header_img", "about_me");
+
+		profilereptest.save(tester);
+
+		Optional<Profile> retrievedProfile = profilectrl.findProfileById(tester.getId());
+
+		assertThat(tester.getFirst_name()).isEqualTo(retrievedProfile.get().getFirst_name());
+
+
 	}
-		
-		
+
 }
