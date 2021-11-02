2a111da38e169e0744b82c28b490cc421de2cacf
alexjohnson9551 <alex.johnson@revature.net>
2021-11-02 16:53:56 -0500

    Authorization token now works

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.revature.controllers;
 
 import com.revature.models.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping(path="/testNoAuth")
-    public ResponseEntity<User> getTestUserNoAuth() {
+    public ResponseEntity<User> getTestUserNoAuth(@AuthenticationPrincipal User user) {
         User returnThis = new User();
         returnThis.setEmail("Poncho_Villa_No_Auth");
+        
+        if(user != null) {
+        	System.out.println("I figured out who I am! " + user.getUid());
+        } else {
+        	System.out.println("NO USER");
+        }
+
         return ResponseEntity.ok(returnThis);
     }
 
     @GetMapping(path="/testWithAuth")
-    public ResponseEntity<User> getTestUserWithAuth() {
+    public ResponseEntity<User> getTestUserWithAuth(@AuthenticationPrincipal User user) {
         User returnThis = new User();
         returnThis.setEmail("Poncho_Villa_With_Auth");
+        
+        System.out.println("I figured out who I am! " + user.getUid());
+        
         return ResponseEntity.ok(returnThis);
     }
 }
