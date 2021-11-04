d098ffc945a2bb42a7d1194174e1a374d5ff6511
Andres Rodriguez <andresmikaelrodriguez@gmail.com>
2021-11-04 08:54:15 -0700

    Comments on user modules

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -4,17 +4,11 @@ import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.User;
-
 
 @RestController
 @RequestMapping("/api/user")
@@ -26,6 +20,8 @@ public class UserController {
     UserController(UserService userService) {
         this.userService = userService;
     }
+
+    /* Registers a new user, only requires a Token in the header, no body is required at the moment */
 
     @PostMapping(path = "/register")
     public ResponseEntity<String> registerUser(@AuthenticationPrincipal User user) {
