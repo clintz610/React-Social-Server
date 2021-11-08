97f8deb9e9175e9865903757ac30d9872a70755d
xaxchkuget <xaxchkuget@gmail.com>
2021-11-08 09:45:54 -0800

    Created profile unit test example

---
+++ src/test/java/com/revature/controllers/TestProfileControllerIntegration.java
@@ -0,0 +1,38 @@
+package com.revature.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.revature.ReverbApplication;
+import com.revature.models.Profile;
+import com.revature.models.User;
+import com.revature.repositories.ProfileRepository;
+
+	@SpringBootTest(classes = ReverbApplication.class)
+	@RunWith(SpringRunner.class)
+	public class TestProfileControllerIntegration {
+	
+		@Autowired
+		private ProfileController profilectrl;
+	
+		@Autowired
+		private ProfileRepository profilereptest;
+		
+		@Test
+		public void findProfileById_integration() throws Exception {
+	
+			ResponseEntity<Profile> retrievedProfile = profilectrl.findProfileById(1);
+	
+			assertThat("Lion").isEqualTo(retrievedProfile.getBody().getFirst_name());
+	
+		}
+
+}
