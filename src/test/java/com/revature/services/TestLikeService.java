e8e165bd60b0abf3fc92157f00b400bdfe7d48f5
Jason Oh <jkoh123@yahoo.com>
2021-11-09 17:40:30 -0500

    completed TestLikeService

--- src/test/java/com/revature/services/TestLikeService.java
+++ src/test/java/com/revature/services/TestLikeService.java
@@ -1,48 +1,22 @@
 package com.revature.services;
 
-import com.revature.models.Comment;
 import com.revature.models.Like;
 import com.revature.models.Post;
-import com.revature.models.Profile;
 import com.revature.models.User;
 import com.revature.repositories.LikeRepository;
 import com.revature.repositories.PostRepository;
-import com.revature.repositories.UserRepository;
-
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.when;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
-import org.assertj.core.api.Fail;
-import org.hibernate.annotations.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.revature.ReverbApplication;
 import com.revature.exceptions.PostNotFoundException;
 
@@ -90,8 +64,33 @@ public class TestLikeService {
 			fail();
 		} catch (Exception e) {
 			assertEquals(e.getClass(), PostNotFoundException.class);
-		}
-		
-		
+		}	
 	}
+
+	@Test
+	public void likePostTest() 
+	{
+		Post post = new Post();
+		User user = new User();
+
+		Mockito.when(postRepository.findById(8L)).thenReturn(Optional.empty());
+		LikeService likeService = new LikeService(postRepository, likeRepository);
+		likeService.likePost(post.getId(), user);
+
+		assertTrue(likeService.checkIfAlreadyLiked(post.getId(), user));
+	}
+
+	@Test
+	public void likeTest() 
+	{
+		Post post = new Post();
+		User user = new User();
+
+		Mockito.when(postRepository.findById(8L)).thenReturn(Optional.empty());
+		LikeService likeService = new LikeService(postRepository, likeRepository);
+		likeService.likePost(post.getId(), user);
+
+		assertTrue(likeService.checkIfAlreadyLiked(post, user));
+	}
+
 }
