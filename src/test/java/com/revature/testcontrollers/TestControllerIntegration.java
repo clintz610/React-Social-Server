e76a117b3e4c28a98d0f07d5edd7d7aeb6eceea3
xaxchkuget <xaxchkuget@gmail.com>
2021-11-09 21:29:33 -0800

    Update TestControllerIntegration.java

--- src/test/java/com/revature/testcontrollers/TestControllerIntegration.java
+++ src/test/java/com/revature/testcontrollers/TestControllerIntegration.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -28,17 +29,32 @@ public class TestControllerIntegration {
 	@Autowired
 	private UserService userservice;
 
+	
+	
 	 @Test
-	 public void devops_profile_test() throws Exception {
-
+	 public void profileCreationTest() throws Exception {
+		 
 		 User testuser = new User("587722673625gfs78dv6987a6svf9","76876v8g7v68@email.com");
 		 
 		 userservice.registerUser(testuser);
 		 
-	 	ResponseEntity<Profile> retrievedProfile = profilectrl.findThisUsersProfile(testuser);
+		 ResponseEntity<Profile> retrievedProfile = profilectrl.findThisUsersProfile(testuser);
+		
+		 assertThat(testuser.getUid()).isEqualTo(retrievedProfile.getBody().getUser().getUid());
+	 	
+	 	
+	}
+	 
+	 @Test
+	 public void updateProfileTest() throws Exception {
+		 
+		 User testuser2 = new User("587722673625gfs78dv6987a6svf7","76876v8g6v68@email.com");
+		 
+		 userservice.registerUser(testuser2);
+		 		 
+		 ResponseEntity<Profile> updatedprofile = profilectrl.updateProfile(profilectrl.findThisUsersProfile(testuser2).getBody(), testuser2);
 
-	 	assertThat(testuser.getUid()).isEqualTo(retrievedProfile.getBody().getUser().getUid());
+		 assertThat(updatedprofile.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
 
 	}
-
 }
