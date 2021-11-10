6a9db1c9342b7fa76590739ec8ae46b716f8146b
xaxchkuget <xaxchkuget@gmail.com>
2021-11-09 20:48:17 -0800

    Update TestControllerIntegration.java

--- src/test/java/com/revature/testcontrollers/TestControllerIntegration.java
+++ src/test/java/com/revature/testcontrollers/TestControllerIntegration.java
@@ -1,45 +1,44 @@
-//package com.revature.testcontrollers;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import com.revature.ReverbApplication;
-//import com.revature.controllers.ProfileController;
-//import com.revature.models.Profile;
-//import com.revature.repositories.ProfileRepository;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ReverbApplication.class)
-////@TestPropertySource(locations = "classpath:application-integration-testing.properties")
-////@ActiveProfiles("test")
-//public class TestControllerIntegration {
-//
-//	@Autowired
-//	private ProfileController profilectrl;
-//
-//	@Autowired
-//	private ProfileRepository profilereptest;
-//
-//	// @Test
-//	// public void devops_profile_test() throws Exception {
-//
-//		Profile tester = new Profile();
-//
-//	// 	profilereptest.save(tester);
-//
-//	// 	ResponseEntity<Profile> retrievedProfile = profilectrl.findProfileById(tester.getId());
-//
-//	// 	assertThat(tester.getFirst_name()).isEqualTo(retrievedProfile.getBody().getFirst_name());
-//
-//		profilereptest.delete(tester);
-//	}
-//
-//}
+package com.revature.testcontrollers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.ReverbApplication;
+import com.revature.controllers.ProfileController;
+import com.revature.models.Profile;
+import com.revature.models.User;
+import com.revature.services.UserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ReverbApplication.class)
+//@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
+public class TestControllerIntegration {
+
+	@Autowired
+	private ProfileController profilectrl;
+	
+	@Autowired
+	private UserService userservice;
+
+	 @Test
+	 public void devops_profile_test() throws Exception {
+
+		 User testuser = new User("587722673625gfs78dv6987a6svf9","76876v8g7v68@email.com");
+		 
+		 userservice.registerUser(testuser);
+		 
+	 	ResponseEntity<Profile> retrievedProfile = profilectrl.findThisUsersProfile(testuser);
+
+	 	assertThat(testuser.getUid()).isEqualTo(retrievedProfile.getBody().getUser().getUid());
+
+	}
+
+}
