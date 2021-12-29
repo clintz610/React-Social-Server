8a098849a6e33792fe40511b3881f4988f5076eb
Danhtran123 <47044112+Danhtran123@users.noreply.github.com>
2021-12-29 13:12:29 -0500

    group model created

--- src/main/java/com/revature/models/Group.java
+++ src/main/java/com/revature/models/Group.java
@@ -1,5 +1,47 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "group")
+@Entity
 public class Group {
+
+    @Id
+    @Column(name = "group_id")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id_fk", referencedColumnName = "user_id")
+    private User owner;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(columnDefinition = "default = 'https://www.windowslatest.com/wp-content/uploads/2017/10/Windows-XP-min.jpg'")
+    private String headerImg;
+
+    @Column(columnDefinition = "default = 'https://i.pinimg.com/originals/ca/f3/93/caf393479404b953bc5368a63c32e4e4.png'")
+    private String profilePic;
+
+    @ManyToMany
+    @JoinColumns({
+            @JoinColumn(name = "user_id_fk", referencedColumnName = "user_id"),
+            @JoinColumn(name = "group_id_fk", referencedColumnName = "group_id")
+    })
+    private List<User> users;
 
 }
