241b8b20c6ef5e59abe7cdbe507969acef3670a5
dario robinson <dariorobinson@macbook-air.lan>
2022-01-12 13:02:53 -0800

    removed /api/search from SecurityConfig and added CrossOrigin to search controller. testing pipeline

--- src/main/java/com/revature/search/SearchController.java
+++ src/main/java/com/revature/search/SearchController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = {"http://reverb-ui-bucket.s3-website-us-west-1.amazonaws.com"})
 @RequestMapping(path = "/api/search")
 public class SearchController {
 
