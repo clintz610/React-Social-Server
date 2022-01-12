3b90205824c4ee007ffce413ea8173ea22d5dadf
dario robinson <dariorobinson@macbook-air.lan>
2022-01-12 12:08:04 -0800

    change cross origin localhost to api link in search controller

--- src/main/java/com/revature/search/SearchController.java
+++ src/main/java/com/revature/search/SearchController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://Reverbapiapplication-env.eba-qjeevjhg.us-west-1.elasticbeanstalk.com")
 @RequestMapping(path = "/api/search")
 public class SearchController {
 
@@ -21,8 +21,4 @@ public class SearchController {
         return searchService.userQuery(query);
     }
 
-    @GetMapping(path= "/2")
-    public SearchResponse query2ByString(@RequestParam(name = "query", required = true) String query) {
-        return searchService.userQuery(query);
-    }
 }
