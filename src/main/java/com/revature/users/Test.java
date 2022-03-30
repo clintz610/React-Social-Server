012dcdfe18f63f0435fcbedd07f136a44641ca88
Adam Lyn <adam273@revature.net>
2022-03-30 17:21:00 -0400

    Test2

---
+++ src/main/java/com/revature/users/Test.java
@@ -0,0 +1,18 @@
+package com.revature.users;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping(path = "/hello")
+    public String test() {
+        return "test endpoint works! :)";
+    }
+
+}
