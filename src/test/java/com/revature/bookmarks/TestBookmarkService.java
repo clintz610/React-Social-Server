4c3d66f898b6cfb9d5dbc8d0697fe187c1e1769a
Cameron Lintz <cameron034@revature.net>
2022-04-13 10:45:35 -0400

    Final commit BookmarkPost tests 38% coverage

--- src/test/java/com/revature/bookmarks/TestBookmarkService.java
+++ src/test/java/com/revature/bookmarks/TestBookmarkService.java
@@ -7,23 +7,21 @@ import com.revature.posts.PostRepository;
 import com.revature.posts.postmeta.PostMeta;
 import com.revature.users.User;
 import com.revature.users.profiles.ProfileRepository;
+import javafx.beans.binding.When;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.internal.stubbing.answers.DoesNothing;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.awt.print.Book;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.LOCAL_DATE_TIME;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest (classes = ReverbApplication.class)
 public class TestBookmarkService {
@@ -34,8 +32,8 @@ public class TestBookmarkService {
     private ProfileRepository mockProfileRepository = Mockito.mock(ProfileRepository.class);
     public UUID post_id = UUID.randomUUID();
     public PostMeta testPostMeta = new PostMeta();
-    public List testComments = new ArrayList ();
-    public Post bookmarkedPost = new Post ();
+    public List testComments = new ArrayList();
+    public Post bookmarkedPost = new Post(post_id, "PostText", "ContentLink", testPostMeta, testComments);
 
 
     @BeforeEach
@@ -71,21 +69,37 @@ public class TestBookmarkService {
     }
 
 
-
     @Test
     public void Bookmarked_Post_gets_Bookmarked() throws PostNotFoundException {
-        Post post = new Post();
         User user = new User();
         Bookmark bookmark = new Bookmark();
         List<Bookmark> array = new ArrayList<Bookmark>();
-        Mockito.when(mockPostRepository.findById(bookmarkedPost.getId())).thenReturn(Optional.of(post));
-        Mockito.when(mockBookmarkRepository.getByPostAndUser(post, user)).thenReturn(array);
+        Mockito.when(mockPostRepository.findById(bookmarkedPost.getId())).thenReturn(Optional.of(bookmarkedPost));
+        Mockito.when(mockBookmarkRepository.getByPostAndUser(bookmarkedPost, user)).thenReturn(array);
         BookmarkService bookmarkService = new BookmarkService(mockPostRepository, mockBookmarkRepository, mockProfileRepository);
+        //Act
         sut.bookmarkPost(bookmarkedPost.getId(), user);
         array.add(bookmark);
-        assertTrue(bookmarkService.checkIfAlreadyBookmarked(post, user));
+        //Assert
+        assertTrue(bookmarkService.checkIfAlreadyBookmarked(bookmarkedPost, user));
     }
-}
+    @Test
+    public void remove_Bookmark_Removes_Bookmark () {
+        //arrange
+        Optional<Post> post = mockPostRepository.findById(post_id);
+        BookmarkService spiedSut = Mockito.spy(sut);
+        User user = new User();
+        spiedSut.checkIfAlreadyBookmarked(bookmarkedPost,user);
+        doReturn(true).when(spiedSut).checkIfAlreadyBookmarked(bookmarkedPost,user);
+       when(mockPostRepository.findById(post_id)).thenReturn(post);
+        try {
+            //act
+            sut.removeBookmark(bookmarkedPost.getId(), user);
+        } catch (PostNotFoundException e) {}
 
+    }
+
+
+    }
 
 
